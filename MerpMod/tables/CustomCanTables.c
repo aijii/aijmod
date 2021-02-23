83e3d017d966d36692949203e458aea7d6a4c67f
aijii <aijii81@gmail.com>
2021-02-23 20:22:17 +1100

    setup some basic canbus datafransfers

--- MerpMod/tables/CustomCanTables.c
+++ MerpMod/tables/CustomCanTables.c
@@ -16,9 +16,11 @@ unsigned short CANBusECAUpdateCount CANDATA = 45;
 //CanMessageSetupStruct ccm06 CANDATA = {.id = 0x180, .ext = 1,	.dlc = 8,	.bus = 0,	.mailBox = 25,	.mcs = mcsReceive, 	.nmc = 1, .rate =   0, .callback = (unsigned long)&canCallbackAEMwideband};
 //CanMessageSetupStruct ccm07 CANDATA = {.id = 0x190,	.ext = 0,	.dlc = 4,	.bus = 0,	.mailBox = 26,	.mcs = mcsReceive, 	.nmc = 1, .rate =   0, .callback = (unsigned long)&canCallbackMK3e85Packet};
 //CanMessageSetupStruct *ccmGroup[8] CANDATA;// = {&ccm00,&ccm01,&ccm02,&ccm03,&ccm04,&ccm05,&ccm06,&ccm07};
-//CanMessageSetupStruct ccm00 CANDATA = {.id = 0x640, .ext = 0,	.dlc = 8,	.bus = 0,	.mailBox = 19,	.mcs = mcsTrans, 	.nmc = 0, .rate =   100, .callback = 0};
 //CanMessageSetupStruct ccm01 CANDATA = {.id = 0x0EC,	.ext = 0,	.dlc = 8,	.bus = 0,	.mailBox = 20,	.mcs = mcsReceive, 	.nmc = 1, .rate =   0, .callback = (unsigned long)&canCallbackECAPacket}; 
-//CanMessageSetupStruct ccm02 CANDATA = {.id = 0x641,	.ext = 0,	.dlc = 2,	.bus = 0,	.mailBox = 21,	.mcs = mcsTrans, 	.nmc = 0, .rate =   100, .callback = 0}; 
+
+
+//CanMessageSetupStruct ccm00 CANDATA = {.id = 0x640, .ext = 0,	.dlc = 8,	.bus = 0,	.mailBox = 19,	.mcs = mcsTrans, 	.nmc = 0, .rate =   100, .callback = 0};
+//CanMessageSetupStruct ccm02 CANDATA = {.id = 0x641, .ext = 0,	.dlc = 5,	.bus = 0,	.mailBox = 21,	.mcs = mcsTrans, 	.nmc = 0, .rate =   100, .callback = 0}; 
 
 
 CanMessageSetupStruct ccm00 CANDATA = {.id = 0x0EC, .ext = 0,	.dlc = 8,	.bus = 0,	.mailBox = 19,	.mcs = mcsReceive, 	.nmc = 1, .rate =   0, .callback = (unsigned long)&canCallbackECAPacket};
@@ -29,12 +31,18 @@ CanMessageSetupStruct ccm04 CANDATA = {.id = 0x000,	.ext = 0,	.dlc = 0,	.bus = 0
 CanMessageSetupStruct ccm05 CANDATA = {.id = 0x000,	.ext = 0,	.dlc = 0,	.bus = 0,	.mailBox = 24,	.mcs = mcsTrans, 	.nmc = 0, .rate =   0, .callback = 0};
 CanMessageSetupStruct ccm06 CANDATA = {.id = 0x000, .ext = 0,	.dlc = 0,	.bus = 0,	.mailBox = 25,	.mcs = mcsTrans, 	.nmc = 0, .rate =   0, .callback = 0};
 CanMessageSetupStruct ccm07 CANDATA = {.id = 0x000,	.ext = 0,	.dlc = 0,	.bus = 0,	.mailBox = 26,	.mcs = mcsTrans, 	.nmc = 0, .rate =   0, .callback = 0};
-CanMessageSetupStruct ccm08 CANDATA = {.id = 0x000,	.ext = 0,	.dlc = 0,	.bus = 0,	.mailBox = 27,	.mcs = mcsTrans, 	.nmc = 0, .rate =   0, .callback = 0};
-CanMessageSetupStruct ccm09 CANDATA = {.id = 0x000,	.ext = 0,	.dlc = 0,	.bus = 0,	.mailBox = 28,	.mcs = mcsTrans, 	.nmc = 0, .rate =   0, .callback = 0};
+CanMessageSetupStruct ccm08 CANDATA = {.id = 0x640,	.ext = 0,	.dlc = 8,	.bus = 0,	.mailBox = 27,	.mcs = mcsTrans, 	.nmc = 0, .rate = 100, .callback = 0};
+CanMessageSetupStruct ccm09 CANDATA = {.id = 0x641,	.ext = 0,	.dlc = 8,	.bus = 0,	.mailBox = 28,	.mcs = mcsTrans, 	.nmc = 0, .rate = 100, .callback = 0};
 CanMessageSetupStruct ccm10 CANDATA = {.id = 0x000,	.ext = 0,	.dlc = 0,	.bus = 0,	.mailBox = 29,	.mcs = mcsTrans, 	.nmc = 0, .rate =   0, .callback = 0};
 CanMessageSetupStruct ccm11 CANDATA = {.id = 0x000,	.ext = 0,	.dlc = 0,	.bus = 0,	.mailBox = 30,	.mcs = mcsTrans, 	.nmc = 0, .rate =   0, .callback = 0};
 
 unsigned char dataLinkedInRam  __attribute__ ((section ("RamHole")));
+
+
+//640: BBBB FFFF  //boost fuel press 
+//641: cifo exxx //temps ethanol
+//642:
+//643:
 
 /*
 unsigned long cmDTaddr[cmDTCount] CANDATA = {
@@ -49,57 +57,58 @@ unsigned long cmDTaddr[cmDTCount] CANDATA = {
 	(unsigned long)&pRamVariables.MapBlendRatio,
 	(unsigned long)&pRamVariables.aemLambda,(unsigned long)&pRamVariables.aemOxygen,(unsigned long)&pRamVariables.aemDataValid,(unsigned long)&pRamVariables.aemSensorFault,
 	(unsigned long)&pRamVariables.rEthanolCAN,(unsigned long)&pRamVariables.tFuelCAN};
-*/	
+*/
+
 
 unsigned long cmDTaddr[cmDTCount] CANDATA = {
-	(unsigned long)pManifoldAbsolutePressure, (unsigned long)&pRamVariables.MapBlendRatio,
-	0,
-	0,
-	0,
-	0,
-	0,
-	0,
-	0,
-	0,
+	(unsigned long)pManifoldRelativePressure, (unsigned long)&pRamVariables.FuelPressureDifferential,
+	(unsigned long)pCoolantTemp,(unsigned long)pIntakeAirTemp,(unsigned long)&pRamVariables.tFuelCAN,(unsigned long)&pRamVariables.rEthanolCAN,
+	(unsigned long)&pRamVariables.WideBandLambda,0,0,0,
 	0,0,0,0,	
-	(unsigned long)&pRamVariables.rEthanolCAN,(unsigned long)&pRamVariables.tFuelCAN};
+	0,0};
 
 
 //format conversion prior to transmitting
 // this controls whether the address table cmdtaddr is checked.
 // it is also the input to the format conversion	
 unsigned char cmDTtypeIn[cmDTCount] CANDATA = {
-	dtUndefined,dtUndefined,
-	dtUndefined,dtUndefined,dtUndefined,dtUndefined,dtUndefined,dtUndefined,dtUndefined,dtUndefined,
+	dtFloat,dtFloat,
+	dtFloat,dtFloat,dtChar,dtChar,
+	dtFloat,dtUndefined,dtUndefined,dtUndefined,
 	dtUndefined,dtUndefined,dtUndefined,dtUndefined,
 	dtUndefined,dtUndefined};
 
 unsigned char cmDTtypeOut[cmDTCount] CANDATA = {
-	dtChar,dtChar,
-	dtChar,dtChar,dtChar,dtChar,dtChar,dtChar,dtChar,dtChar,
+	dtFloat,dtFloat,
+	dtChar,dtChar,dtChar,dtChar,
+	dtFloat,dtChar,dtChar,dtChar,
 	dtChar,dtChar,dtChar,dtChar,
 	dtChar,dtChar};
 	
 unsigned char cmDTccm[cmDTCount] CANDATA = {
-	1,1,
-	8,8,8,8,8,8,8,8,
+	8,8,
 	9,9,9,9,
-	2,2};
+	9,0,0,0,
+	0,0,0,0,
+	0,0};
 	
 unsigned char cmDTpos[cmDTCount] CANDATA = {
-	0,1,
-	0,1,2,3,4,5,6,7,
+	0,4,
 	0,1,2,3,
-	0,1};
+	4,0,0,0,
+	0,0,0,0,
+	0,0};
 	
 float cmDTscale[cmDTCount] CANDATA = {
 	0,0,
-	0,0,0,0,0,0,0,0,
+	0,0,0,0,
+	0,0,0,0,
 	0,0,0,0,0,0};
 	
 float cmDToffset[cmDTCount] CANDATA = {
 	0,0,
-	0,0,0,0,0,0,0,0,
+	0,0,0,0,
+	0,0,0,0,
 	0,0,0,0,0,0};
 
 #endif
